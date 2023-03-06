6b6edbc112af6179189b2606883f53d2dabcfc2c
Michael-Mehta <supermichael2001@gmail.com>
2023-03-05 19:21:14 -0700

    added icons to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,21 +1,25 @@
 import React from 'react'
+import { BsFillHouseDoorFill, BsSearch, BsCompass,
+  BsChatText, BsHeart, BsPlusSquare} from 'react-icons/bs';
+ import { BiMoviePlay } from "react-icons/bi";
+ import { GiDove, GiHamburgerMenu } from "react-icons/gi";
 
 const NavBar = () => {
 
   return (
     <div className='naVbar'>
         <div className='naVItems'>
-          <h1></h1>
+          <h1>Instagram</h1>
 
-        <div>Home</div>
-        <div>Search</div>
-        <div>Explore</div>
-        <div>Reels</div>
-        <div>Messages</div>
-        <div>Notifications</div>
-        <div>Create</div>
-        <div>Profile</div>
-        <div>More</div>
+        <div><div><BsFillHouseDoorFill/></div><div>Home</div></div>
+        <div><div><BsSearch/></div><div>Search</div></div>
+        <div><div><BsCompass/></div><div>Explore</div></div>
+        <div><div><BiMoviePlay/></div><div>Reels</div></div>
+        <div><div><BsChatText/></div><div>Messages</div></div>
+        <div><div><BsHeart/></div><div>Notifications</div></div>
+        <div><div><BsPlusSquare/></div><div>Create</div></div>
+        <div><div>Profile</div></div>
+        <div><div><GiHamburgerMenu/></div><div>More</div></div>
         </div>
 
     </div>

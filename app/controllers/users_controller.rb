8f9d763564b8450fd1afa74f51291f5294d738f2
Michael <mmehta1@wgu.edu>
2025-08-12 15:00:55 -0700

    added nil checks to the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,64 +1,77 @@
 class UsersController < ApplicationController
+  include ActiveStorage::SetCurrent
 
-    include ActiveStorage::SetCurrent
-    
-    def show
-        @user = User.find_by(id: params[:id])
-        followers = @user.followers
+  before_action :authenticate_user!, only: [:suggested_users]
 
-         follower_list = []
-         followers.each do |follower|
-        follower_list << {
+  def show
+    @user = User.find_by(id: params[:id])
+
+    if @user.nil?
+      render json: { error: "User not found" }, status: :not_found and return
+    end
+
+    followers = @user.followers || []
+    follower_list = followers.map do |follower|
+      {
         id: follower.id,
         username: follower.username,
         avatar_url: url_for(follower.avatar)
       }
-
     end
 
-
-      following = @user.following
-
-         following_list = []
-         following.each do |followee|
-        following_list << {
+    following = @user.following || []
+    following_list = following.map do |followee|
+      {
         id: followee.id,
         username: followee.username,
         avatar_url: url_for(followee.avatar)
       }
     end
-        render json: { id: @user.id, email: @user.email, username: @user.username, avatar_url: url_for(@user.avatar),
-         followers: follower_list, following: following_list }
-      end
 
+    render json: { 
+      id: @user.id,
+      email: @user.email,
+      username: @user.username,
+      avatar_url: url_for(@user.avatar),
+      followers: follower_list,
+      following: following_list
+    }
+  end
 
-      def update_avatar
-        user = User.find(params[:id])
-        user.avatar.attach(params[:avatar])
-        render json: { message: 'Avatar updated successfully' }
-      end
-    
-      def posts
-        @user = User.find(params[:id])
-        posts = @user.posts
-        render json: posts.map { |post| post.as_json(include: :image).merge(image: post.image.url) }
-      end
+  def update_avatar
+    user = User.find_by(id: params[:id])
+    if user.nil?
+      render json: { error: "User not found" }, status: :not_found and return
+    end
 
+    user.avatar.attach(params[:avatar])
+    render json: { message: 'Avatar updated successfully' }
+  end
 
-      def suggested_users
-         # Get a list of all users except the current user and those the current user is already following
-         @users = User.where.not(id: current_user.id).where.not(id: current_user.following.pluck(:id))
+  def posts
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      render json: { error: "User not found" }, status: :not_found and return
+    end
 
-         # Randomly select 4 users from the list
-         @users = @users.order("RANDOM()").limit(4)
+    posts = @user.posts
+    render json: posts.map { |post| post.as_json(include: :image).merge(image: post.image.url) }
+  end
 
-         render json: @users.map { |user| 
-         {
-           username: user.username, id: user.id, avatar_url: url_for(user.avatar)
-           } 
-          }
-        
-      end
-     
-    
+  def suggested_users
+    unless current_user
+      render json: { error: "Unauthorized" }, status: :unauthorized and return
+    end
+
+    users = User.where.not(id: current_user.id).where.not(id: current_user.following.pluck(:id))
+    users = users.order("RANDOM()").limit(4)
+
+    render json: users.map { |user| 
+      {
+        username: user.username,
+        id: user.id,
+        avatar_url: url_for(user.avatar)
+      }
+    }
+  end
 end
